62d9cb3f00f4db14c8bca658e3541cdbe9a19ca0
Marcelo Cumbanez <marcelocumbanez6@gmail.com>
2023-09-11 14:28:40 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -6,6 +6,9 @@ const HelloWorld = () => {
       <View style={styles.centeredView}>
         <Text style={styles.text}>Hello World</Text>
       </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>Desenvolvido por Marcelo Cumbane</Text>
+      </View>
     </SafeAreaView>;
 };
 
@@ -21,6 +24,17 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 20
+  },
+  footer: {
+    width: '100%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 0
+  },
+  footerText: {
+    fontSize: 16
   }
 });
 export default HelloWorld;
